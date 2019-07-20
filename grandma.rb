9eea8ac07fcb282480f313a618e57e22a2cc4ca6
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-20 04:44:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
  speak_to_grandma ("I LOVE YOU TOO GRANDMA!")
  
-  if speak_to_grandma == "I LOVE YOU GRANDMA!"
-   puts "I LOVE YOU TOO PUMPKIN!"
+  if speak_to_grandma != speak_to_grandma("I love you too pumpkin!").upcase
+   return "I LOVE YOU TOO PUMPKIN!"
 elsif speak_to_grandma == "Hi Nana, how are you?"
    puts "HUH?! SPEAK UP, SONNY!"
  elsif speak_to_grandma == "Hi"
