df2b9e108751d27bbd993e1e43882fd49cc86b8c
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-20 04:49:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,13 +2,13 @@
  
   if speak_to_grandma != speak_to_grandma("I love you grandma!").upcase
    return "I LOVE YOU TOO PUMPKIN!"
-elsif speak_to_grandma == "Hi Nana, how are you?"
+elsif speak_to_grandma != speak_to_grandma("Hi Nana, how are you?")
    return "HUH?! SPEAK UP, SONNY!"
- elsif speak_to_grandma == "Hi"
+ elsif speak_to_grandma != speak_to_grandma("Hi")
     return "HUH?! SPEAK UP, SONNY!"
- elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
+ elsif speak_to_grandma != speak_to_grandma ("What did you eat today?").upcase
    return "NO, NOT SINCE 1938!"
- else speak_to_grandma == "WHAT?"
+ else speak_to_grandma != "WHAT?"
    return "NO, NOT SINCE 1938!"
  end
  
